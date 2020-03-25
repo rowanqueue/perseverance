1db4ab5706378ce8a6b29e7a73439cbdf46b5637
rowanq <rowanqueue@gmail.com>
2020-03-24 21:06:43 -0400

    initial commit

---
+++ Assets/Scripts/Rover.cs
@@ -0,0 +1,122 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public enum Command : byte{
+    Up,
+    Right,
+    Down,
+    Left,
+    PickUp,
+    PutDown
+}
+public class Rover : MonoBehaviour
+{
+    public bool waitingForInput;
+    public int numMoves;
+    public TaskManager taskManager = new TaskManager();
+    public List<MoveRover> moves = new List<MoveRover>();
+    bool doMoves = false;
+    float elapsedTime;
+
+    public TextMeshPro text;
+
+    void Update()
+    {
+        waitingForInput = !doMoves;
+        taskManager.Update();
+        if(doMoves){
+            elapsedTime+=Time.deltaTime;
+            //taskManager.Update();
+            if(elapsedTime >= numMoves*0.25f){
+                doMoves = false;
+                text.text = "";
+            }
+        }else{
+            if(moves.Count >= numMoves){
+                doMoves = true;
+                elapsedTime = 0;
+                //fill commands here
+                var move = moves[0];
+                for(int i = 1; i < numMoves; i++){
+                    moves[i-1].Then(moves[i]);
+                }
+                taskManager.Do(moves[0]);
+                moves.Clear();
+            }else{
+                if(Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)){
+                    EnterCommand(Command.Up);
+                }
+                if(Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow)){
+                     EnterCommand(Command.Right);
+                }
+                if(Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow)){
+                     EnterCommand(Command.Down);
+                }
+                if(Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow)){
+                     EnterCommand(Command.Left);
+                }
+            }
+        }
+        
+    }
+    public void EnterCommand(Command command){
+        if(waitingForInput){
+            switch(command){
+                case Command.Up:
+                    moves.Add(new MoveRover(this, Vector2.up));
+                    text.text +="Up ";
+                    break;
+                case Command.Right:
+                    moves.Add(new MoveRover(this, Vector2.right));
+                    text.text +="Right ";
+                    break;
+                case Command.Left:
+                    moves.Add(new MoveRover(this, Vector2.left));
+                    text.text +="Left ";
+                    break;
+                case Command.Down:
+                    moves.Add(new MoveRover(this, Vector2.down));
+                    text.text +="Down ";
+                    break;
+            }
+        }
+        
+    }
+    public class MoveRover : Task
+    {
+        private float duration = 1.0f;
+        private Rover rover;
+        private float elapsedTime = 0;
+        public Vector2 direction;
+        Vector2 target;
+        Vector2 start;
+        public MoveRover(Rover rover, Vector2 direction)
+        {
+            this.direction = direction;
+            this.rover = rover;
+            duration = 0.25f;
+            elapsedTime = 0f;
+        }
+        protected override void Initialize(){
+            start = (Vector2)rover.transform.position;
+            target = (Vector2)rover.transform.position+direction;
+            target.x = Mathf.RoundToInt(target.x);
+            target.y = Mathf.RoundToInt(target.y);
+        }
+        internal override void Update(){
+            elapsedTime += Time.deltaTime;
+            if(elapsedTime >= duration){
+                Debug.Log("E");
+                SetStatus(TaskStatus.Success);
+                return;
+            }
+            rover.transform.position = Vector2.Lerp(start,target,elapsedTime/duration);
+        }
+        protected override void OnSuccess(){
+            Debug.Log("SUCCESS");
+            rover.transform.position = target;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Task.cs
@@ -0,0 +1,175 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Task {	
+	public enum TaskStatus : byte 
+	{     
+		Detached, // Task has not been attached to a TaskManager     
+		Pending, // Task has not been initialized     
+		Working, // Task has been initialized     
+		Success, // Task completed successfully     
+		Fail, // Task completed unsuccessfully     
+		Aborted // Task was aborted 
+	}  
+
+	public TaskStatus Status { get; private set; }  
+
+	public bool IsDetached { get { return Status == TaskStatus.Detached;}}
+	public bool IsAttached { get { return Status != TaskStatus.Detached;}}
+	public bool IsPending { get { return Status == TaskStatus.Pending; } }
+	public bool IsWorking { get { return Status == TaskStatus.Working; } }
+	public bool IsSuccessful {get{ return Status == TaskStatus.Success;}} 
+	public bool IsFailed { get { return Status == TaskStatus.Fail; } }
+	public bool IsAborted { get { return Status == TaskStatus.Aborted; } } 
+	public bool IsFinished { get { return (Status == TaskStatus.Fail || Status == TaskStatus.Success || Status == TaskStatus.Aborted); } }  
+
+	public void Abort() 
+	{     
+		SetStatus(TaskStatus.Aborted); 
+	}
+
+	internal void SetStatus(TaskStatus newStatus) 
+	{     
+		if (Status == newStatus) return;      
+		Status = newStatus;      
+		switch (newStatus)     
+		{
+			case TaskStatus.Working:             
+				Initialize();             
+				break;
+			case TaskStatus.Success:
+				OnSuccess();             
+				CleanUp();             
+				break;          
+			case TaskStatus.Aborted:             
+				OnAbort();             
+				CleanUp();             
+				break;
+			case TaskStatus.Fail:             
+				OnFail();             
+				CleanUp();             
+				break;          
+			case TaskStatus.Detached:
+			case TaskStatus.Pending:
+				break;
+			default:
+				throw new System.ArgumentOutOfRangeException(newStatus.ToString(), newStatus, null); 
+		} 
+	}
+
+	protected virtual void OnAbort() {}
+	protected virtual void OnSuccess() {}  
+	protected virtual void OnFail() {}
+
+	protected virtual void Initialize() { }  
+	internal virtual void Update() { }  
+	protected virtual void CleanUp() { }
+
+	public Task NextTask { get; set; }  
+
+	public Task Then(Task task) 
+	{     
+		Debug.Assert(!task.IsAttached);     
+		this.NextTask = task;     
+		Debug.Log(NextTask);
+		return task; 
+	}
+
+}
+
+public class ActionTask : Task {
+	private readonly System.Action _action;
+
+	public ActionTask(System.Action action = null)     
+	{         
+		_action = action ?? (() => { });     
+	}      
+
+	protected override void Initialize()     
+	{         
+		SetStatus(TaskStatus.Success);       
+		_action();
+	} 
+}
+
+public class DelegateTask : Task
+{
+	public delegate void InitializationHandler();
+	public delegate bool UpdateHandler();
+	public delegate void SuccessHandler();
+	public delegate void FailureHandler();
+
+	private InitializationHandler initialize;
+	private UpdateHandler update;
+	private SuccessHandler onSuccess;
+	private FailureHandler onFailure;
+
+	public DelegateTask(InitializationHandler initialize, UpdateHandler update = null, SuccessHandler onSuccess = null, FailureHandler onFailure = null)
+	{
+		this.initialize = initialize;
+		this.update = update ?? (() => true);
+
+		this.onSuccess = onSuccess;
+		this.onFailure = onFailure;
+	}
+
+	protected override void Initialize()
+	{
+		initialize();
+	}
+
+	internal override void Update()
+	{
+		var finished = update();
+		
+		if (finished) 
+			SetStatus(TaskStatus.Success);
+	}
+
+	protected override void OnSuccess()
+	{
+		onSuccess?.Invoke();
+	}
+
+	protected override void OnFail()
+	{
+		onFailure?.Invoke();
+	}
+}
+
+public class WaitTask : Task 
+{    
+	private readonly double _duration;     
+	private double _elapsedTime;
+
+	public WaitTask(double duration)
+	{
+		_duration = duration;
+	}
+
+	public WaitTask (float duration)
+	{
+		_duration = duration;
+	}
+
+	public WaitTask(int duration)     
+	{         
+		_duration = duration;
+	}     
+
+	protected override void Initialize()     
+	{         
+		_elapsedTime = 0;     
+	}      
+
+	internal override void Update()     
+	{         
+		_elapsedTime += Time.deltaTime;    
+
+		if (_elapsedTime >= _duration)         
+		{             
+			SetStatus(TaskStatus.Success);         
+		}     
+	} 
+}
