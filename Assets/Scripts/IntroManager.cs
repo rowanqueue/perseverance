57c4e8a7c97b71f2fa53b1e4afa1d22b25e2eaaa
ElizabethBallou <epb267@nyu.edu>
2020-04-13 19:33:02 -0400

    added basic intro scene

---
+++ Assets/Scripts/IntroManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using TMPro;
+using UnityEngine.UI;
+
+public class IntroManager : MonoBehaviour
+{
+
+    public SpriteRenderer titlePicture;
+
+    public SpriteRenderer controlRoomPicture;
+
+    public SpriteRenderer blackBackground;
+
+    public Button playButton;
+
+    public TextMeshProUGUI[] titleTextArray;
+
+    private TextMeshProUGUI playButtonText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playButtonText = playButton.gameObject.GetComponentInChildren<TextMeshProUGUI>();
+        controlRoomPicture.DOFade(0f, .1f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onPlayButtonPress()
+    {
+        titlePicture.DOFade(0f, 1f).OnComplete(() => titlePicture.gameObject.SetActive(false));
+        playButton.image.DOFade(0f, 1f).OnComplete(() => playButton.gameObject.SetActive(false));
+        playButtonText.DOFade(0f, 1f).OnComplete(() => tutorialSetup());
+        foreach (var titleAsset in titleTextArray)
+        {
+            titleAsset.DOFade(0f, 1f);
+        }
+    }
+
+    void tutorialSetup()
+    {
+        controlRoomPicture.DOFade(1f, 1f);
+    }
+}
