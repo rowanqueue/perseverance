f2f15910b8bd269676ed45e2b87aecdf8b0989df
ElizabethBallou <epb267@nyu.edu>
2020-05-05 17:04:49 -0400

    added options menu + level screen

---
+++ Assets/Scripts/LevelSelectButtonClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelSelectButtonClick : MonoBehaviour
+{
+    public int buttonNumber;
+    public LevelLoader myLevelLoader;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void onButtonClick()
+    {
+        myLevelLoader.LoadLevel(buttonNumber);
+    }
+}
